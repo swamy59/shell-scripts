80fabd2b282237547b539893761fc8ee93dd6915
swamy59 <65354634+swamy59@users.noreply.github.com>
2024-07-11 22:55:58 +0530

    Delete areaofrectagle.sh

--- areaofrectagle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "enter length: " l
-read -p "enter width: " w
-area=$((l*w))
-echo area of rectangle is $area
-echo addition is of numbers is $((l+w))
-echo sub is $(($l-$w))
-echo div is $((l/w))
